74c25c483e75e5a6490d277ede322737a0486544
azbusinessbooster-ui <azbusinessbooster@gmail.com>
2025-12-15 16:38:32 -0700

    "Add layout.tsx in correct path

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Business Booster",
+  description: "Grow Your Business with Smart Automation & Design",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} m-0 p-0`}>{children}</body>
+    </html>
+  );
+}
